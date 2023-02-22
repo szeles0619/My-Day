b0c8b62e1a4a1df2adf4d6282ffc9c3612182c7c
Istvan <szeles0619@gmail.com>
2023-02-22 11:35:53 +0000

    I added second API - (REST country)

--- script.js
+++ script.js
@@ -37,3 +37,25 @@ document.querySelector(".search button")
     weather.search();
 });
 
+//queryURL = "https://restcountries.com/v2/name/${countryName}"
+
+let searchBtn = document.getElementById("search-btn");
+
+let countryInp = document.getElementById("country-inp");
+
+searchBtn.addEventListener("click", () => {
+    // let countryName = "India";
+    let finalURL = 'https://restcountries.com/v3.1/name/Hungary?fullText=true';
+    console.log(finalURL); 
+    fetch(finalURL)
+    .then((response) => response.json())
+    .then((data) => {
+        //Get informations of the country ---- console.log(data);
+        console.log(data[0]);
+        console.log(data[0].capital[0]);
+        console.log(data[0].continents[0]);
+        console.log(data[0].flags.svg);
+        console.log(data[0].borders);
+        console.log(data[0].population);
+    });
+});
