b3924744b3d46668ecd681e5e6158d9decfb545f
Istvan <szeles0619@gmail.com>
2023-02-22 11:49:04 +0000

    Updated HTML

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ let searchBtn = document.getElementById("search-btn");
 let countryInp = document.getElementById("country-inp");
 
 searchBtn.addEventListener("click", () => {
-    // let countryName = "India";
+    // let countryName = "Hungary";
     let finalURL = 'https://restcountries.com/v3.1/name/Hungary?fullText=true';
     console.log(finalURL); 
     fetch(finalURL)
